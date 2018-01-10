7263ce82cd994eca6c07e795a2699de4d287c950
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2018-01-10 19:23:43 +0700

    Make focus cursor visible with raycaster

--- index.vr.js
+++ index.vr.js
@@ -13,8 +13,17 @@ export default class tictactoe_game extends React.Component {
     super(props)
 
     this.state = {
-      message: 'Welcome to VR Tictactoe'
+      message: 'Player X Turn',
+      focus: false
     }
+  }
+
+  boxFocused() {
+    this.setState({ message: 'Button 1 clicked', focus: true })
+  }
+
+  boxLeave() {
+    
   }
 
   render() {
@@ -27,124 +36,104 @@ export default class tictactoe_game extends React.Component {
 
       title: {
         fontSize: 0.5,
+        position: 'absolute',
+        top: -1,
+        left: '30%'
+      },
+
+      container: {
+        position: 'relative',
         layoutOrigin: [0.5, 0.5],
-        transform: [{ translate: [0.2, 2.3, -9] }]
+        transform: [{ translate: [-1, 0.5, -9] }],
+        flex: 1,
+        backgroundColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: 'auto'
+      },
+
+      board: {
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        height: 'auto',
+        width: 8
+      },
+
+      box: {
+        backgroundColor: 'gold',
+        margin: 0.1,
+        height: 2,
+        width: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+      boxFocus: {
+        backgroundColor: 'gold',
+        borderColor: 'crimson',
+        margin: 0.1,
+        height: 2,
+        width: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
       }
+    }
+
+    let boxStyle;
+
+    if (this.state.focus) {
+      boxStyle = styles.box
+    } else {
+      boxStyle = styles.boxFocus
     }
 
     return (
       <View>
         <Pano source={asset('winter.jpg')}/>
+        
+        <View style={styles.container}>
+          <Text style={styles.title}>{this.state.message}</Text>
+          <View style={styles.board}>
+            <VrButton style={boxStyle} onEnter={() => this.boxFocused()}>
+              <Text style={styles.boardSymbol}> 1 </Text>
+            </VrButton>
 
-        <Text style={styles.title}>{this.state.message}</Text>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 2 clicked' })}>
+              <Text style={styles.boardSymbol}> 2 </Text>
+            </VrButton>
 
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{translate: [-1, 2, -9]}]
-        }} onClick={() => this.setState({
-          message: 'Button 1 clicked'
-        })}> 
-          <Text style={styles.boardSymbol}> 1 </Text> 
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 3 clicked' })}>
+              <Text style={styles.boardSymbol}> 3 </Text>
+            </VrButton>
 
-         <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{translate: [0.2, 3, -9]}]
-        }} onClick={() => this.setState({
-          message: 'Button 2 clicked'
-        })}> 
-          <Text style={styles.boardSymbol}> 2 </Text> 
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 4 clicked' })}>
+              <Text style={styles.boardSymbol}> 4 </Text>
+            </VrButton>
 
-         <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{translate: [1.4, 4, -9]}]
-        }} onClick={() => this.setState({
-          message: 'Button 3 clicked'
-        })}> 
-          <Text style={styles.boardSymbol}> 3 </Text> 
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 5 clicked' })}>
+              <Text style={styles.boardSymbol}> 5 </Text>
+            </VrButton>
 
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [-1, 3.8, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 4 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 4 </Text>
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 6 clicked' })}>
+              <Text style={styles.boardSymbol}> 6 </Text>
+            </VrButton>
 
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [0.2, 4.8, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 5 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 5 </Text>
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 7 clicked' })}>
+              <Text style={styles.boardSymbol}> 7 </Text>
+            </VrButton>
 
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [1.4, 5.8, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 6 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 6 </Text>
-        </VrButton>
-
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [-1, 5.6, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 7 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 7 </Text>
-        </VrButton>
-
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [0.2, 6.6, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 8 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 8 </Text>
-        </VrButton>
-
-        <VrButton style={{
-          width: 1,
-          height: 1,
-          backgroundColor: '#ccc', // textAlignVertical: 'center',
-          layoutOrigin: [0.5, 0.5],
-          transform: [{ translate: [1.4, 7.6, -9] }]
-        }} onClick={() => this.setState({
-          message: 'Button 9 clicked'
-        })}>
-          <Text style={styles.boardSymbol}> 9 </Text>
-        </VrButton>
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 8 clicked' })}>
+              <Text style={styles.boardSymbol}> 8 </Text>
+            </VrButton>
+            
+            <VrButton style={boxStyle} onEnter={() => this.setState({ message: 'Button 9 clicked' })}>
+              <Text style={styles.boardSymbol}> 6 </Text>
+            </VrButton>
+          </View>
+        </View>
+        
       </View>
     );
   }
--- vr/client.js
+++ vr/client.js
@@ -4,10 +4,21 @@
 
 // Auto-generated content.
 import {VRInstance} from 'react-vr-web';
+const SimpleRaycaster = {
+  getType: () => "simple",
+  getRayOrigin: () => [0, 0, 0],
+  getRayDirection: () => [0, 0, -1],
+  drawsCursor: () => true
+};
 
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'tictactoe_game', parent, {
     // Add custom options here
+    raycasters: [
+      SimpleRaycaster // Add SimpleRaycaster to the options
+    ],
+    cursorVisibility: 'visible',
+    enableHotReload: true,
     ...options,
   });
   vr.render = function() {
